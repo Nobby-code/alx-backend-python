a6d0b697715fad873912d0353a8bb99a1d6a8cbd
Nobby-code <norbertguda0@gmail.com>
2025-05-21 15:30:21 +0300

    decorator that logs database queries executed by any function

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,44 @@
+import sqlite3
+import functools
+from datetime import datetime
+
+#### decorator to lof SQL queries
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query_info = f"Execution start: [{datetime.now()}] Executing function: {func.__name__}"
+        print(query_info)
+
+        '''Create Users table'''
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+
+        create_table = '''CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                age INTEGER NOT NULL)
+                '''
+        with sqlite3.connect('users.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute(create_table)
+            conn.commit()
+
+        query_result = func(*args, **kwargs)
+        print(f"Execution end: [{datetime.now()}] Executing function: {func.__name__}\n")
+        return query_result
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
