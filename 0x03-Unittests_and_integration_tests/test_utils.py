5be275d5200071d87a36fec6ba4fcdcb4e3e3b3a
Nobby-code <norbertguda0@gmail.com>
2025-05-30 00:50:51 +0300

    method to test that the method returns what it is supposed to.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Unit testing.
+"""
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test case for the access_nested_map function.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the correct value."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
