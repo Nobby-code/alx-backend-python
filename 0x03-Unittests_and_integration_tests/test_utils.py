afe3a1237aaba47b39aff4d896b11733dcdd5ae8
Nobby-code <norbertguda0@gmail.com>
2025-05-30 01:10:49 +0300

    ssertRaises context manager to test that a KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,9 +18,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that access_nested_map returns the correct value."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """Test that access_nested_map raises KeyError with the correct key.
+        """
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(err.exception.args[0], expected_key)
+
 if __name__ == '__main__':
     unittest.main()
-
 
 
 
