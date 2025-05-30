a003c860a998277ca58b9db36fd45df0071e04bf
Nobby-code <norbertguda0@gmail.com>
2025-05-30 23:29:25 +0300

    class and implement the test_org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Unit tests for GithubOrgClient"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for GithubOrgClient.org"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test that GithubOrgClient.org returns the correct value """
+
+        # Setup expected response and mock
+        expected_result = {"login": org_name}
+        mock_get_json.return_value = expected_result
+
+        # Instantiate the client and call org
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        # Assertions
+        self.assertEqual(result, expected_result)
+        url_org = "https: //api.github.com/orgs/"
+        mock_get_json.assert_called_once_with(f"{url_org}{org_name}")
+
+
+if __name__ == '__main__':
+    unittest.main()
